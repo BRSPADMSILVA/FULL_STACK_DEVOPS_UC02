7b10fe08c5f7a6d0733904def280165e4ec59bd2
ADILSON-MASILVA <ADILSON-MASILVA>
2024-03-27 21:55:43 -0300

    Commit_27-03-24_01

--- uc04_dfs-main/05_sequelize/db/conn.js
+++ uc04_dfs-main/05_sequelize/db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('nodesequelize', 'root', '', {
+const sequelize = new Sequelize('nodesequelize', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
 })
